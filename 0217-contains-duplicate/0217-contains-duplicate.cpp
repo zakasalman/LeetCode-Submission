f2b99374ae6ec44b82d122b49c7b004ac05fc251
ZAKA <154753212+zakasalman@users.noreply.github.com>
2026-07-24 20:55:28 +0530

    76 ms (23.31%) 111.1 MB (34.1%) 07-24-2026 0217-contains-duplicate .cpp Easy

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> frq ;
-        for(int i : nums) frq[i] ++ ;
-        for(auto &p : frq){
-            if(p.second != 1) return true ;
+        // using unordered set 
+        unordered_set<int> s ;
+        int n  = nums.size() ;
+        for(int num : nums){
+            if(s.count(num) > 0){
+                return true ;
+            }
+            s.insert(num) ;
         }
         return false ;
     }
