863d671c15d15d549a384fb51c1a0d47f9b5b63e
ZAKA <154753212+zakasalman@users.noreply.github.com>
2026-07-26 17:11:58 +0530

    0 ms (100%) 7.8 MB (35.59%) 07-26-2026 0371-sum-of-two-integers .cpp Medium

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a + b ; 
+        while(b != 0){
+            int carry = (a & b) << 1 ; // calculate carry
+            a = a ^ b ; // sum without carry
+            b = carry ; // update carry
+        }
+        return a ;
     }
 };
